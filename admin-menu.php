d720369dd981d331ff5e628388c9e897b19e0034
Aaron T. Grogg <aarontgrogg@gmail.com>
2013-04-04 06:47:41 +0200

    Hopefully the last of the inconsistent naming conventions

--- admin-menu.php
+++ admin-menu.php
@@ -3,8 +3,8 @@
 /*
 	Plugin Name: Enhanced Admin Links in Multisite 'My Sites' Drop-Downs
 	Plugin URI: http://aarontgrogg.com/2013/03/26/wordpress-plugin-enhanced-admin-links-in-multisite-my-sites-drop-downs/
-	Description: For multisite installations, adds 'Posts', 'Pages', 'Themes', 'Plugins', 'Settings', and 'Tools' links to all of the Admin 'My Sites' drop-down menus.
-	Version: 1.2
+	Description: For multisite installations, adds 'Posts', 'Pages', 'Themes', 'Plugins', 'Tools', and 'Settings' links to all of the Admin 'My Sites' drop-down menus.
+	Version: 1.5
 	Author: Aaron T. Grogg
 	Author URI: http://aarontgrogg.com/
 	License: GPLv2 or later
@@ -15,37 +15,52 @@
 
 		function add_links_to_my_sites() {
 
-			// make sure user is allowed to do this
+			// Make sure user is allowed to do this
 			if (current_user_can('manage_network')) {
 
-				// grab a couple variables...
+				// Grab a couple variables...
 				global $wp_admin_bar;
 				$all_nodes = $wp_admin_bar->get_nodes();
-				$links_to_add = array( 'Posts', 'Pages', 'Themes', 'Plugins', 'Settings', 'Tools' );
-				$i = -1;
+				$links_to_add = array( 'Posts', 'Pages', 'Themes', 'Plugins', 'Tools', 'Settings' );
 
-				// loop through all nodes
+				// Loop through all nodes
 				foreach ($all_nodes as $node) {
 
-					++$i;
-					// if we encounter a Dashboard, we want to add the new links
+					// If we encounter a Dashboard, we want to add the new links
 					if ($node->title === 'Dashboard') {
-						// grab a couple variables...
-						$parent = $node->parent;
-						$href = $node->href;
 
-						// loop through and add the new menu links
+						// Loop through and add the new menu links
 						foreach ($links_to_add as $link) {
-							// Network doesn't have a POsts, Pages, or Tools page, so skip them
+
+							$href = $node->href;
+
+							// Network doesn't have a Posts, Pages, or Tools page, so skip them
 							if (strpos($href, 'wp-admin/network') && ($link === 'Posts' || $link === 'Pages' || $link === 'Tools')) {
 								continue;
 							}
-							// for everything else, push the new link to the menu array
+
+							// Grab a couple variables...
+							$parent = $node->parent;
+							$lc = strtolower($link);
+							$id = $parent . '-' . $lc;
+
+							// There are a few problem children, the rest play nicely
+							if ($link === 'Posts') {
+								$href = $href . 'edit.php';
+							} else if ($link === 'Pages') {
+								$href = $href . 'edit.php?post_type=page';
+							} else if ($link === 'Settings' && !strpos($href, 'wp-admin/network')) {
+								$href = $href . 'options-general.php';
+							} else {
+								$href = $href . $lc . '.php';
+							}
+
+							// For everything else, push the new link to the menu array
 							$wp_admin_bar->add_menu( array(
 								'parent' => $parent,
-								'id' => $parent . '-' . strtolower($link),
+								'id' => $id,
 								'title' => $link,
-								'href' => $href . strtolower($link) . '.php',
+								'href' => $href,
 							));
 
 						} // foreach
@@ -60,7 +75,7 @@
 
 	} // if ( !function_exists( 'add_links_to_my_sites' ) )
 
-	// if we're in admin mode, add action to admin_bar_menu hook
+	// If we're in admin mode, add action to admin_bar_menu hook
 	if (is_admin()) {
 		add_action('admin_bar_menu', 'add_links_to_my_sites', 200);
 	} // is_admin
